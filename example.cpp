fe95f8f3513a0bc86538ca374b61673c20087e3a
Sethvan <sethvan@yahoo.com>
2022-11-28 19:32:48 -0700

    added to example.cpp

--- example.cpp
+++ example.cpp
@@ -22,6 +22,20 @@ int main() {
               << setcolor( Color::CYAN ) << "of " << setcolor( Color::MAGENTA ) << "words " << setcolor( Color::YELLOW )
               << "with " << setcolor( Color::RED ) << "different " << setcolor( Color::GREEN ) << "colors." << '\n';
     std::cout << setcolor( Color::YELLOW_BACK ) << str1 << setcolor( Color::RED_BACK ) << str2 << std::endl;
-
+    std::cout << setcolor( Color::RED ) << "RED" << '\n';
+    std::cout << setcolor( Color::GREY ) << "GREY" << '\n';
+    std::cout << setcolor( Color::GREEN ) << "GREEN" << '\n';
+    std::cout << setcolor( Color::YELLOW ) << "YELLOW" << '\n';
+    std::cout << setcolor( Color::BLUE ) << "BLUE" << '\n';
+    std::cout << setcolor( Color::MAGENTA ) << "MAGENTA" << '\n';
+    std::cout << setcolor( Color::CYAN ) << "CYAN" << '\n';
+    std::cout << setcolor( Color::DARK_GREY ) << "DARK_GREY" << '\n';
+    std::cout << setcolor( Color::BLACK_BACK ) << "BLACk BACKGROUND" << '\n';
+    std::cout << setcolor( Color::RED_BACK ) << "RED BACKGROUND" << '\n';
+    std::cout << setcolor( Color::GREEN_BACK ) << "GREEN BACKGROUND" << '\n';
+    std::cout << setcolor( Color::YELLOW_BACK ) << "YELLOW BACKGROUND" << '\n';
+    std::cout << setcolor( Color::BLUE_BACK ) << "BLUE BACKGROUND" << '\n';
+    std::cout << setcolor( Color::MAG_BACK ) << "MAGENTA BACKGROUND" << '\n';
+    std::cout << setcolor( Color::CYAN_BACK ) << "CYAN BACKGROUND" << '\n';
     return 0;
 }
